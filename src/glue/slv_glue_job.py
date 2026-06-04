d96c916f5a4bfd1978da24e755fe49db459db2c6
Lisandrot1 <lmlisandro3@gmail.com>
2026-06-03 23:02:00 -0500

    feat: inicio de transformacion de glue

--- src/glue/slv_glue_job.py
+++ src/glue/slv_glue_job.py
@@ -1,1 +1,25 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from pyspark import SparkContext
+from awsglue.job import Job
+from pyspark.sql.types import StringType, IntegerType, DoubleType, DateType
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql.functions import F
 
+sc = SparkContext()
+gluecontext = GlueContext(sc)
+spark = gluecontext.spark_session
+job = Job(gluecontext)
+job.init()
+
+
+
+df = gluecontext.create_dynamic_frame.from_catalog(
+    database="",
+    table_name=""
+)
+
+
+job.commit()
