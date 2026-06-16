d802d4d70f1a9dea93dca9313f1e3d612a51a439
Lisandrot1 <lmlisandro3@gmail.com>
2026-06-16 10:51:47 -0500

    feat: slv de inventory listo

--- src/glue/slv_glue_job.py
+++ src/glue/slv_glue_job.py
@@ -4,7 +4,7 @@ from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
 from pyspark import SparkContext
 from awsglue.job import Job
-from pyspark.sql.types import StringType, IntegerType, DoubleType, DateType
+from pyspark.sql.types import StringType, IntegerType, DoubleType, DateType, FloatType
 from awsglue.dynamicframe import DynamicFrame
 from pyspark.sql import functions as F
 
@@ -31,7 +31,51 @@ for tables in TABLES:
 
 def slv_inventory(dyf: F.DataFrame) -> F.DataFrame:
     df = dyf.toDF()
+    df = df.select([
+        F.col("event_id"),
+        F.col("actor.id").alias("actor_id"),
+        F.col("actor.role").alias("actor_role"),
+        F.coalesce(
+            F.col("details.change_delta.int"),
+            F.col("details.change_delta.double")).cast(IntegerType()).alias("change_delta"),
+        F.coalesce(
+            F.col("details.new_value.int"),
+            F.col("details.new_value.double")).cast(IntegerType()).alias("new_value"),
+        F.coalesce(
+            F.col("details.previous_value.int"),
+            F.col("details.previous_value.double")).cast(IntegerType()).alias("previous_value"),
+        F.col("details.product_id").alias("product_id"),
+        F.col("details.unit").alias("unit"),
+        F.col("event_type"),
+        F.col("level"),
+        F.col("metadata.reason").alias("reason"),
+        F.col("metadata.section").alias("section"),
+        F.col("metadata.warehouse_id").alias("warehouse_id"),
+        F.col("service"),
+        F.col("at").cast(DateType()).alias("date_event")
+    ])
+    
+    df = df.filter(F.col("event_id").isNotNull())
+    
+    for columns in df.schema:
+        if isinstance(columns.dataType, (IntegerType, FloatType, DoubleType)):
+            df = df.withColumn(
+                columns.name,
+                F.when(F.col(columns.name) < 0, F.abs(F.col(columns.name)))
+                .otherwise(columns.name)
+            )
+    
+    fill_integer = [col.name for col in df.schema if isinstance(df.dataType, (IntegerType, FloatType, DoubleType))]
+    fill_string = [col.name for col in df.schema if isinstance(df.dataType, (StringType))]
+    
+    fill_values = {col: 0 for col in fill_integer}
+    fill_values.update({col: "UNKNOWN" for col in fill_string})
+    
+    df = df.fillna(fill_values)
     return df
+
+
+
 def slv_orders(dyf: F.DataFrame) -> F.DataFrame:
     df = dyf.toDF()
     return df
